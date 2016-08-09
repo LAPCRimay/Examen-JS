262210b76b1a695cd142c7ec5fde19550c1202bf
LAPCRimay <lorepadilla0@gmail.com>
2016-08-09 17:49:07 -0500

    Examen JS

---
+++ Pokemon/pruebatwj/assets/Aplicacion/Vistas/InicioCtrl.js
@@ -0,0 +1,5 @@
+aplicacion.controller("InicioCtrl",function($scope){
+
+    $scope.titulo = "INGRESO POKEMONES"
+
+});
---
+++ Pokemon/pruebatwj/assets/Aplicacion/app.js
@@ -0,0 +1,18 @@
+var aplicacion = angular.module("pokemon", ['ui.router']);
+
+aplicacion.config(function ($stateProvider, $urlRouterProvider) {
+
+    $urlRouterProvider.otherwise("/");
+
+    $stateProvider
+        .state('inicio', {
+            url: "/"
+            , templateUrl: "Vistas/Inicio.html"
+            , controller: "InicioCtrl"
+        })
+        .state('pokemon', {
+            url: "/pokemon"
+            , templateUrl: "Vistas/Pokemon.html"
+            , controller: "PokemonCtrl"
+        });
+});
